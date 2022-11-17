ac8092d70ec0c21823819fbccccfb0310c680f12
Armin <armin-burger@gmx.de>
2022-11-17 14:46:25 +0100

    inital commit

---
+++ crypt.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta charset="utf-8">
+
+
+<?php
+
+require_once 'vendor/autoload.php';
+
+function generate_password ($length, $digits, $special) {
+    $letters = 'abcdefghijklmnopqrstuvwxyz';
+    $lettersUpper = strtoupper($letters);
+    $digitsChars = '1234567890';
+$specialChars = '#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+
+    $factory = new RandomLib\Factory;
+    $generator = $factory->getMediumStrengthGenerator();
+    $options = '';
+
+    if ($digits) {
+        if ($special) {
+            $options = $letters . $lettersUpper . $specialChars;
+        } else {
+            $options = $letters . $lettersUpper . $digitsChars;
+        }
+    } else {
+        if ($special) {
+            $options = $letters . $lettersUpper . $puntion;
+        } else {
+            $options = $letters . $lettersUpper;
+        }
+    }   
+    
+    $randpass = $generator->generateString($length, $options);
+    return utf8_encode($randpass);
+}
+
+function generate_userid () {
+
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $userid = array();
+    $alphaLength = strlen($alphabet) - 1;
+    for ($i = 0; $i < 12; $i++) {
+        $n = rand(0, $alphaLength);
+        $userid[] = $alphabet[$n];
+    }
+    return implode($userid);
+}
+
+function hash_pw ($string) {
+
+    $password = utf8_encode($string);
+    $hashed = password_hash($password, PASSWORD_DEFAULT);
+    return $hashed;
+}
+
+function check_pw ($string, $hash) {
+    
+    return password_verify($string, $hash);
+}
