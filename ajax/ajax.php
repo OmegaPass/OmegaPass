6ddebdd79abee47d2750194affa91d35468f9008
Armin Burger <armin-burger@gmx.de>
2023-03-09 20:37:57 +0100

    create two ajax endpoints for password generate and stregth

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -1,6 +1,15 @@
 <?php
 include "../db.php";
+include_once "../crypt.php";
 
 if (isset($_GET['getPass'])) {
     echo json_encode(get_all_entries(getUserId())[$_GET['getPass']]);
+}
+
+if ($_POST['generate']) {
+    echo generate_password($_POST['length'], $_POST['numbers'], $_POST['special']);
+}
+
+if ($_POST['passwordStregth'] && is_string($_POST['password'])) {
+    echo check_password_strength($_POST['password']);
 }
