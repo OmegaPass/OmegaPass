eb94948a49cbf3498b344a9dae3f073ce1bf34f7
Armin Burger <armin-burger@gmx.de>
2023-04-10 11:47:48 +0200

    add column date when moved to trash

--- db-schema/pwmanager.sql
+++ db-schema/pwmanager.sql
@@ -14,7 +14,8 @@ CREATE TABLE `passwords` (
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
    `id` varchar(32) PRIMARY KEY DEFAULT (UUID()),
-   `trash` BOOLEAN DEFAULT NULL
+   `trash` BOOLEAN DEFAULT NULL,
+   `trashDate` datetime default null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `users` (
