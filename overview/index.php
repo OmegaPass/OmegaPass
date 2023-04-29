173953cb21c94c91f055e56c162b7192663e360e
Armin Burger <armin-burger@gmx.de>
2023-04-29 10:34:24 +0200

    fix: add missing font

--- overview/index.php
+++ overview/index.php
@@ -80,6 +80,7 @@ switch ($_GET['mode']) {
         <title>OmegaPass</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="../css/overview.css">
     </head>
     <body>
