8cfec8c6cfeb3efcc3c330367b3a55a6c0521063
Armin Burger <armin-burger@gmx.de>
2023-05-08 19:22:06 +0000

    feat: move gif after being logged in

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,3 @@
 $(document).ready(function() {
-    $('.welcome-gif-wrapper').fadeIn(200);
 
-    setTimeout(() => {
-        let fadeOutTime = 200;
-        $('.welcome-gif-wrapper').fadeOut(fadeOutTime);
-
-        setTimeout(() => $('.welcome-gif-wrapper').hide(), fadeOutTime);
-    }, 3510)
 });
