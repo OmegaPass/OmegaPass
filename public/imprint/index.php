ef8db2b00b18cfcce5804ba7895b55f952f88235
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2023-05-11 00:37:04 +0200

    feat: add imprint

---
+++ public/imprint/index.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OmegaPass</title>
+    <link rel="stylesheet" href="../css/imprint.css">
+</head>
+<body>
+    <a href="/overview/">
+        <img id="home-button" alt="overview button" src="../omegapass.jpg"/>
+    </a>
+    <div id="imprint">
+        <h1>Impressum</h1>
+
+        <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+        <h3>Herausgeber</h3>
+        <p>OStD Dr. Joachim Selzam, Schulleiter<br />
+        Goldbergstra&szlig;e 5<br />
+        Staatliche Berufsschule Lichtenfels<br />
+        96215 Lichtenfels</p>
+
+        <h3>Kontakt</h3>
+        <p>Telefon: 09571 9574-0<br />
+        Telefax: 09571 9574-29<br />
+        E-Mail: 5019.verwaltung@schule.bayern.de</p>
+
+
+        <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+        <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+        <p>Quelle: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a></p>
+    </div>
+</body>
+</html>
